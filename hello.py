0d05bd884892a4f1610608eaef252c908ee2cee5
Guilherme Casalle de Souza <guilherme_casalle@hotmail.com>
2024-11-04 16:09:09 -0300

    Caneca-Commerce

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!<p>"
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run()
